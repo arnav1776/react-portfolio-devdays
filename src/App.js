4de60d83cdd333d49ab87db8524a862af539a6ad
Arnav Sharma <arnav1776@gmail.com>
2021-09-02 11:42:36 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import { Flex, VStack, Heading} from "@chakra-ui/react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <VStack>
+      <Flex w="100%">
+        <Heading
+         ml="8" size="md" fontWeight="semibold" color="cyan.400">Arnav Sharma</Heading>
+      </Flex>
+    </VStack>
   );
 }
 
